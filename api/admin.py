efb9e5579ef9a05b0d3e9f13b758ea4713d08956
R3hetwal <rakshya.bhetwal34@gmail.com>
2023-04-16 22:30:08 +0545

    Phase II

--- api/admin.py
+++ api/admin.py
@@ -6,10 +6,6 @@ import xlwt
 
 # Register your models here.
 
-# class ProjectAdmin(admin.ModelAdmin):
-#     list_display = ('project_name', 'description', 'start_date', 'complete')
-#     list_filter = ('start_date', 'complete')
-
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('project_name', 'description', 'start_date', 'complete')
     list_filter = ('start_date', 'complete')
@@ -44,9 +40,39 @@ class ProjectAdmin(admin.ModelAdmin):
     export_as_xls.short_description = "Export selected as XLS"
 
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('document_name', 'created_at', 'identifier')
+    list_display = ('document_name', 'content', 'created_at', 'identifier')
     list_filter = ('created_at',)
     search_fields = ('identifier',)
+    actions = ['export_as_csv', 'export_as_xls']
+
+    def export_as_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="documents.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['Document Name', 'Content', 'Created At', 'Identifier'])
+        for document in queryset:
+            writer.writerow([document.document_name, document.content, document.identifier, document.created_at])
+        return response
+    export_as_csv.short_description = "Export selected as CSV"
+
+    def export_as_xls(self, request, queryset):
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="documents.xls"'
+        wb = xlwt.Workbook(encoding='utf-8')
+        ws = wb.add_sheet('Projects')
+        row_num = 0
+        columns = ['Document Name', 'Content', 'Created At', 'Identifier']
+        for col_num, column_title in enumerate(columns):
+            ws.write(row_num, col_num, column_title)
+        for document in queryset:
+            row_num += 1
+            row = [document.document_name, document.content, document.identifier, document.created_at]
+            for col_num, cell_value in enumerate(row):
+                ws.write(row_num, col_num, cell_value)
+        wb.save(response)
+        return response
+    export_as_xls.short_description = "Export selected as XLS"
+
 
 class DepartmentAdmin(admin.ModelAdmin):
     list_display = ('department_name', 'department_head')
@@ -55,5 +81,3 @@ class DepartmentAdmin(admin.ModelAdmin):
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Document, DocumentAdmin)
 admin.site.register(Department, DepartmentAdmin)
-
-#testcaaaaaaase completed 
